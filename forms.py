be70daa8432d48da56766eb5efdf800960522f75
MachineCreation <142163578+MachineCreation@users.noreply.github.com>
2024-01-30 17:38:30 -0800

    forms update 1 of 2

--- forms.py
+++ forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class UserLoginForm(FlaskForm):
-    first_name = StringField('First name', validators = [DataRequired()])
-    last_name = StringField('First name', validators = [DataRequired()])
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
-    favorite_liquor = StringField('favorite liquor', validators= [DataRequired()])
+    first_name = StringField('First name')
+    last_name = StringField('First name')
+    favorite_liquor = StringField('favorite liquor')
     submit_button = SubmitField()
